f1227bb63de468dc6d871ee5492d8bb694488275
Dimitrije Jankovic <131003887+dimitrijejank@users.noreply.github.com>
2026-04-21 13:55:53 +0200

    hirst_dot_painting_commit
    used colorgram for color extraxtion from image

--- maina.py
+++ maina.py
@@ -1,45 +1,79 @@
-from turtle import Turtle, Screen
+import colorgram
+import turtle as turtle_module
 import random
 
-olovka = Turtle()
+turtle_module.colormode(255)
+# turtle_module.colormode(255) - colormode pravi problem
+colors = colorgram.extract("slika1.jpg", 100)
+
+olovka = turtle_module.Turtle()
+# ekran = turtle_module.Screen()
 olovka.penup()
-olovka.goto(x=230,y = 130)
-olovka.pensize(3)
-olovka.pendown()
-olovka.goto(x=230,y = -130)
-olovka.penup()
-ekran = Screen()
-ekran.setup(height=400,width=550)
-izbor_boja = ekran.textinput(title="Make a bet!", prompt="Which turtle will win the race? Enter a color:")
-colors = ["red", "orange", "yellow", "blue", "green", "purple"]
-y_positions = [-70, -40, -10, 20, 50, 80]
-sve_kornjace = []
+olovka.speed("fast")
+olovka.setheading(0)
+olovka.setheading(225)
+olovka.forward(300)
+olovka.setheading(0)
+olovka.hideturtle()
+# ekran.exitonclick()
+num_of_dots = 100
 
-for turtle_index in range (0,6):
-    kor1 = Turtle(shape="turtle")
-    kor1.color(colors[turtle_index])
-    kor1.penup()
-    kor1.goto(x=-200, y=y_positions[turtle_index])
-    sve_kornjace.append(kor1)
-
-if izbor_boja:
-    is_race_on = True
-
-while is_race_on:
-    for turtle in sve_kornjace:
-        if turtle.xcor() > 230:
-            is_race_on = False
-            if izbor_boja == turtle.pencolor():
-                ekran.title("YOU WON")
-                print("YOU WON!!!")
-            else:
-                ekran.title("YOU LOST")
-                print(f"You lost, the winner is {turtle.pencolor} turtle!")
-
-        random_dist = random.randint(0, 10)
-        turtle.forward(random_dist)
+# boje = []
+boje = [(246, 244, 243), (235, 240, 246), (247, 240, 243), (240, 246, 243), (133, 164, 202), (225, 150, 101), (30, 43, 64), (201, 136, 148), (163, 59, 49), (236, 212, 88), (44, 101, 147), (136, 181, 161), (148, 64, 72), (51, 41, 45), (161, 32, 29), (60, 115, 99), (59, 48, 45), (170, 29, 32), (215, 83, 73), (236, 167, 157), (230, 163, 168), (36, 61, 55), (15, 96, 71), (33, 60, 106), (172, 188, 219), (194, 99, 108), (106, 126, 158), (18, 83, 105), (175, 200, 188), (35, 150, 209), (65, 66, 56), (103, 140, 129), (164, 200, 214), (130, 129, 122)]
 
 
 
-print(izbor_boja)
-ekran.exitonclick()
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     nova = (r,g,b)
+#     boje.append(nova)
+
+# print(boje)
+# olovka.dot(20,(248, 245, 237))
+
+for broj in range(1, num_of_dots + 1):
+    olovka.dot(20,random.choice(boje))
+    olovka.forward(50)
+
+    if broj % 10 == 0:
+        olovka.setheading(90)
+        olovka.forward(50)
+        olovka.setheading(180)
+        olovka.forward(500)
+        olovka.setheading(0)
+
+
+
+
+
+
+
+
+
+
+
+
+#KOD ZA EXTRACT BOJA IZ SLIKE PREKO COLORGRAM.PY
+# rgb_colors = []
+# hsl_colors = []
+# colors = colorgram.extract("slika.jpg", 10)
+
+# for color in colors:
+#     rgb_colors.append(color.rgb)
+#     hsl_colors.append(color.hsl)
+#
+# print(rgb_colors)
+# print(hsl_colors)
+
+# color_tuples = []
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     color_tuples.append(new_color)
+#
+# print(color_tuples)
